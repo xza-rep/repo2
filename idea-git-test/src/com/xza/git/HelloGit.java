098641a91248729ba89a26b5c33b2bb288d3ef6e
xza <xza_aln@163.com>
2018-11-15 18:31:40 +0800

    将测试工程添加到git

---
+++ idea-git-test/src/com/xza/git/HelloGit.java
@@ -0,0 +1,19 @@
+package com.xza.git;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
